f6f0353fa92c02647e484c1f2b436f22cac8bb38
Ginder Singh <ginder@windscribe.com>
2025-03-10 03:11:30 -0400

    apply version flag.

--- build_lib.sh
+++ build_lib.sh
@@ -11,52 +11,32 @@
 # usage: $ ./build_lib.sh v1.3.4
 
 TAG="$1"
-# Hacky way to replace version info.
-update_versionInfo() {
-    local file="$1/ctrld/cmd/cli/cli.go"
-    local tag="$2"
-    local commit="$3"
-    awk -v tag="$tag" -v commit="$commit" '
-        BEGIN { version_updated = 0; commit_updated = 0 }
-        /^\tversion/ {
-            sub(/= ".+"/, "= \"" tag "\"");
-            version_updated = 1;
-        }
-        /^\tcommit/ {
-            sub(/= ".+"/, "= \"" commit "\"");
-            commit_updated = 1;
-        }
-        { print }
-        END {
-            if (version_updated == 0) {
-                print "\tversion = \"" tag "\"";
-            }
-            if (commit_updated == 0) {
-                print "\tcommit = \"" commit "\"";
-            }
-        }
-    ' "$file" > "$file.tmp" && mv "$file.tmp" "$file"
-}
 export PATH=$PATH:~/go/bin
 mkdir bin
 cd bin || exit
 root=$(pwd)
-# Get source from github and switch to tag
+
+# Get source from GitHub and switch to the specified tag
 git clone --depth 1 --branch "$TAG" https://github.com/Control-D-Inc/ctrld.git
+
 # Prepare gomobile tool
 sourcePath=./ctrld/cmd/ctrld_library
 cd $sourcePath || exit
 go install golang.org/x/mobile/cmd/gomobile@latest
 go get golang.org/x/mobile/bind
 gomobile init
+
 # Prepare build info
 buildDir=$root/../build
 mkdir -p "$buildDir"
 COMMIT=$(git rev-parse HEAD)
-update_versionInfo "$root" "$TAG" "$COMMIT"
-ldflags="-s -w"
+
+# Set linker flags with version and commit info
+ldflags="-s -w -X gitlab.int.windscribe.com/controld/clients/ctrld.git/cmd/cli.version=$TAG -X gitlab.int.windscribe.com/controld/clients/ctrld.git/cmd/cli.commit=$COMMIT"
+
 # Build
 gomobile bind -ldflags="$ldflags" -o "$buildDir"/ctrld.aar || exit
+
 # Clean up
 rm -r "$root"
-echo "Successfully built Ctrld library $TAG($COMMIT)."
+echo "Successfully built Ctrld library $TAG ($COMMIT)."
